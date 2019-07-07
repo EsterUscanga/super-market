5e1ccdc1429b5adc2afec292e0fa5021e8bce5ce
EsterUscanga <ester.uscanga@cceo.com.mx>
2019-07-06 19:02:40 -0500

    Was added a validation to just use letters in product and category

--- index.js
+++ index.js
@@ -18,25 +18,37 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
   let response = { message: "" }
-  const categoryExistAlready = await categoryExist(req.body.name)
 
-  if (categoryExistAlready) {
-
+  if (!onlyLetters(req.body.name, req.body.products[0].name))
+    response.message = "Use just letters please"
+  else {
+    const categoryExistAlready = await categoryExist(req.body.name)
     const productExistAlready = await productExist(req.body.products[0].name)
 
-    if (productExistAlready)
-      response.message = "Product has been created already"
-    else {
-      await setProduct(req)
-      response.message = "Product created"
+
+    if (categoryExistAlready) {
+
+
+      if (productExistAlready)
+        response.message = "Product has been created already"
+      else {
+        await setProduct(req)
+        response.message = "Product created"
+      }
+    } else {
+      await setCategory(req)
+      response.message = "Category with product created"
     }
-  } else {
-    await setCategory(req)
-    response.message = "Category with product created"
   }
   res.send(response)
 })
 
+function onlyLetters(product, category) {
+  if (product.search('[^a-zA-Z]+') == -1 && category.search('[^a-zA-Z]+') == -1)
+    return true
+  else
+    return false
+}
 const printMessage = () => { console.log('Running at http://localhost:3000/') }
 
 app.listen(3000, printMessage)  
